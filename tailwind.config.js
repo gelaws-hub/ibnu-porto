7e6a522b71273fc3b9c250047668ea0fb3bcafc3
ibnu fadhil <fadhilgelaws@gmail.com>
2025-04-08 15:07:43 +0700

    remove border radius configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -55,11 +55,6 @@ export const theme = {
         foreground: "hsl(var(--card-foreground))",
       },
     },
-    borderRadius: {
-      lg: "var(--radius)",
-      md: "calc(var(--radius) - 2px)",
-      sm: "calc(var(--radius) - 4px)",
-    },
     fontFamily: {
       sans: ["Inter", "sans-serif"],
     },
